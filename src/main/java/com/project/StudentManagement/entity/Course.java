978c1ce38ec6d632323f291c12400aba2afa6db9
vaibhav <vaibhav.waghmare@techprescient.com>
2024-01-29 12:07:22 +0530

    Entities Added

---
+++ src/main/java/com/project/StudentManagement/entity/Course.java
@@ -0,0 +1,47 @@
+package com.project.StudentManagement.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "COURSE_TBL")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "course_seq")
+    @SequenceGenerator(name = "course_seq", sequenceName = "course_tbl_seq", allocationSize = 1)
+    private Integer id;
+
+    @Column(name = "title", nullable = false)
+
+    private String title;
+
+    @Column(name = "course_code", nullable = false)
+
+    private double courseCode;
+
+    @Column(name = "credits", nullable = false)
+
+    private int credits;
+
+    @Column(name = "fee",nullable = false)
+    private double fee;
+    //    @Column(name = "Students")
+
+
+    @ManyToMany( fetch = FetchType.LAZY,  mappedBy = "courses")
+//    @JsonBackReference
+    private Set<Student> students = new HashSet<>(); // Initialize the set
+
+}
---
+++ src/main/java/com/project/StudentManagement/entity/Student.java
@@ -0,0 +1,51 @@
+package com.project.StudentManagement.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "STUDENT_TBL")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_seq")
+    @SequenceGenerator(name = "student_seq", sequenceName = "student_tbl_seq", allocationSize = 1)
+    private Integer id;
+
+    @Column(name="name")
+
+    private String name;
+
+    @Column(name = "academic_year")
+
+    private String year;
+
+    @Column(name="department")
+
+    private String dept;
+    //    @Column(name = "Courses")
+
+    @ManyToMany( fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "student_course_table",
+            joinColumns = {
+                    @JoinColumn(name = "student_id")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "course_id")
+            })
+
+//    @JsonManagedReference
+    private Set<Course> courses;
+
+}
