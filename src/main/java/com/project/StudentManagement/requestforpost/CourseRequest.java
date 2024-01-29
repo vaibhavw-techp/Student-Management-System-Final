7510478bb97a60bb3a70ff9a3ae6e5c90fe287c8
vaibhav <vaibhav.waghmare@techprescient.com>
2024-01-29 12:09:43 +0530

    Liquibase Added

---
+++ src/main/java/com/project/StudentManagement/requestforpost/CourseRequest.java
@@ -0,0 +1,41 @@
+package com.project.StudentManagement.requestforpost;
+
+public class CourseRequest {
+        private String title;
+        private Double courseCode;
+        private Integer credits;
+        private Double fee;
+
+        public String getTitle() {
+                return title;
+        }
+
+        public void setTitle(String title) {
+                this.title = title;
+        }
+
+        public Double getCourseCode() {
+                return courseCode;
+        }
+
+        public void setCourseCode(Double courseCode) {
+                this.courseCode = courseCode;
+        }
+
+        public Integer getCredits() {
+                return credits;
+        }
+
+        public void setCredits(Integer credits) {
+                this.credits = credits;
+        }
+
+        public Double getFee() {
+                return fee;
+        }
+
+        public void setFee(Double fee) {
+                this.fee = fee;
+        }
+}
+
