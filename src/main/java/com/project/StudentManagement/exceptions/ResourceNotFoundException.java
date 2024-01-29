b7d01460f13daf089e233be331775eb2f32e860b
vaibhav <vaibhav.waghmare@techprescient.com>
2024-01-29 12:07:51 +0530

    Exceptions Added

---
+++ src/main/java/com/project/StudentManagement/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.project.StudentManagement.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception{
+    public ResourceNotFoundException(Integer id){
+
+        super("404 not found with id: "+id);
+    }
+}
