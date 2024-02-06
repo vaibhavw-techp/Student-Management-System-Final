ef11506212d3682e6860f82e71a56c15a6bbbb7a
vaibhav <vaibhav.waghmare@techprescient.com>
2024-02-06 10:20:05 +0530

    Update Student DTO is added

---
+++ src/main/java/com/project/StudentManagement/dto/UpdateStudentDTO.java
@@ -0,0 +1,30 @@
+package com.project.StudentManagement.dto;
+
+
+
+import com.project.StudentManagement.requestforpost.CourseRequest;
+import com.project.StudentManagement.dto.CourseDTO;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateStudentDTO
+{
+    private String fullName;
+    private String year;
+    private String dept;
+
+//    private Set<CourseDTO> courses;
+
+
+}
